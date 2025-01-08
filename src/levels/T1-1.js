70842202674b6faf6e00ddf554bba16ccbabf17b
dpbarry <deanbarry100@gmail.com>
2025-01-08 17:45:19 -0500

    Straighten out

--- src/levels/T1-1.js
+++ src/levels/T1-1.js
@@ -3,5 +3,5 @@
         COLS = 3;
         DOMAIN = [1,2,3,4];
         SOLUTION = [[null,2,null],[3,null,1],[null,4,null]];
-        NEXT_LEVEL = "T1.2";
+        NEXT_LEVEL = "T1-2";
        
--- src/levels/T1-4.js
+++
@@ -1,7 +0,0 @@
-
-        ROWS = 2;
-        COLS = 5;
-        DOMAIN = [4,5];
-        SOLUTION = [[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]];
-        NEXT_LEVEL = "4";
-       

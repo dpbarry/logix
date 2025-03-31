5cb0f07b3e2318a91473fecb693d6c39272a48e5
dpbarry <deanbarry100@gmail.com>
2025-03-31 13:04:13 -0400

    Adjust info wording. Todo - withold unlocking of matrix plus

--- src/levels/T1-5.js
+++ src/levels/T1-5.js
@@ -1,7 +1,7 @@
 
         ROWS = 3;
-        COLS = 2;
-        DOMAIN = [0,1];
-        SOLUTION = [[null,0],[1,0],[1,null]];
+        COLS = 3;
+        DOMAIN = [1,2,3];
+        SOLUTION = [[3,null,2],[1,3,null],[null,2,1]];
         NEXT_LEVEL = "T1-6";
        

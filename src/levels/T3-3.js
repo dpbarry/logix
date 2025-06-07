ab4e9a8d5c1566f435e1d73e4e5c4935f4bd6c64
dpbarry <deanbarry100@gmail.com>
2025-06-07 14:20:37 +0800

    Add level

--- src/levels/T3-3.js
+++ src/levels/T3-3.js
@@ -2,6 +2,6 @@
         ROWS = 3;
         COLS = 2;
         DOMAIN = [1,2,3,4];
-        SOLUTION = [[null],[],[]];
+        SOLUTION = [[4,3],[3,2],[2,1]];
         NEXT_LEVEL = "T3-4";
        
---
+++ src/levels/T3-4.js
@@ -0,0 +1,7 @@
+
+        ROWS = 3;
+        COLS = 3;
+        DOMAIN = [0,1,2,3];
+        SOLUTION = [[null,0,1],[0,3,0],[1,0,null]];
+        NEXT_LEVEL = "T3-5";
+       
